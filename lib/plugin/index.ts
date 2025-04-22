b99cc3f098ccda76f3cf29ed02b0af0f5bdf7d94
Julian <chetkara@gmail.com>
2025-04-22 14:49:49 +0200

    moved remaining stuff out of inidex.css to the plugin

--- lib/plugin/index.ts
+++ lib/plugin/index.ts
@@ -4,6 +4,39 @@ import anchor from "./components/anchor";
 import button from "./components/button";
 import utilities from "./utilities/utilities";
 
-export default createPlugin(({ addBase, addComponents, addUtilities }) => {
-  addBase(base), addComponents([anchor, button]), addUtilities(utilities);
-});
+export default createPlugin(
+  ({ addBase, addComponents, addUtilities, addVariant }) => {
+    addBase(base),
+      addComponents([anchor, button]),
+      addUtilities(utilities),
+      addVariant("selected", "&[data-selected]");
+  },
+  {
+    theme: {
+      extend: {
+        colors: {
+          main: "var(--color-main)",
+          "main-content": "var(--color-main-content)",
+          light: "var(--color-light)",
+          "light-content": "var(--color-light-content)",
+          dark: "var(--color-dark)",
+          "dark-content": "var(--color-dark-content)",
+          primary: "var(--color-primary)",
+          "primary-content": "var(--color-primary-content)",
+          secondary: "var(--color-secondary)",
+          "secondary-content": "var(--color-secondary-content)",
+          accent: "var(--color-accent)",
+          "accent-content": "var(--color-accent-content)",
+          info: "var(--color-info)",
+          "info-content": "var(--color-info-content)",
+          success: "var(--color-success)",
+          "success-content": "var(--color-success-content)",
+          warning: "var(--color-warning)",
+          "warning-content": "var(--color-warning-content)",
+          error: "var(--color-error)",
+          "error-content": "var(--color-error-content)",
+        },
+      },
+    },
+  },
+);
