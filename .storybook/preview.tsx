3e3446b604cd0001da39d4bddfdc001860ca1505
Julian <chetkara@gmail.com>
2025-04-30 22:05:23 +0300

    dark mode toggle for docs

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,12 @@
-import type { Preview } from "@storybook/react";
+import React from "react";
+import { DocsContainer } from "@storybook/blocks";
+import { addons } from "@storybook/preview-api";
 import { themes } from "@storybook/theming";
+import { DARK_MODE_EVENT_NAME } from "storybook-dark-mode";
+import type { Preview } from "@storybook/react";
 import "../lib/index.css";
+
+const channel = addons.getChannel();
 
 const preview: Preview = {
   parameters: {
@@ -13,8 +19,25 @@ const preview: Preview = {
       light: themes.light,
       stylePreview: true,
     },
+
     docs: {
-      theme: themes.dark,
+      container: ({ children, context }) => {
+        const [isDark, setDark] = React.useState();
+
+        React.useEffect(() => {
+          channel.on(DARK_MODE_EVENT_NAME, setDark);
+          return () => channel.removeListener(DARK_MODE_EVENT_NAME, setDark);
+        }, [channel, setDark]);
+
+        return (
+          <DocsContainer
+            context={context}
+            theme={isDark ? themes.dark : themes.normal}
+          >
+            {children}
+          </DocsContainer>
+        );
+      },
     },
   },
 };
