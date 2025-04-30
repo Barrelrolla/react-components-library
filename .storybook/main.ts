0e490adf67fe6b1fe9f57da242481ed5e99baa36
Julian <chetkara@gmail.com>
2025-04-30 20:26:30 +0300

    added dark mode toggle to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,10 +3,11 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../lib/**/*mdx", "../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-essentials",
+    { name: "@storybook/addon-essentials", options: { backgrounds: false } },
     "@storybook/experimental-addon-test",
     "@storybook/addon-themes",
     "@storybook/addon-a11y",
+    "storybook-dark-mode",
   ],
   framework: {
     name: "@storybook/react-vite",
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,36 +1,22 @@
-import React from "react";
-import { withThemeByClassName } from "@storybook/addon-themes";
-import type { Preview, ReactRenderer } from "@storybook/react";
-import { DocsPage } from "@storybook/addon-docs";
+import type { Preview } from "@storybook/react";
 import { themes } from "@storybook/theming";
 import "../lib/index.css";
 
 const preview: Preview = {
   parameters: {
     options: { storySort: { order: ["Intro", ["Welcome"], "Components"] } },
-    backgrounds: {
-      default: "dark",
-    },
     layout: "fullscreen",
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+    darkMode: {
+      classTarget: "html",
+      dark: themes.dark,
+      darkClass: ["dark"],
+      light: themes.light,
+      stylePreview: true,
     },
     docs: {
       theme: themes.dark,
     },
   },
-  decorators: [
-    withThemeByClassName<ReactRenderer>({
-      themes: {
-        light: "",
-        dark: "dark",
-      },
-      defaultTheme: "dark",
-    }),
-  ],
 };
 
 export default preview;
