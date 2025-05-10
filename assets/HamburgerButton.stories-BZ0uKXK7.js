af490379068c157694478a99c6fdf0305abf9cfa
Julian <chetkara@gmail.com>
2025-05-10 20:10:10 +0300

    Updates

--- assets/HamburgerButton.stories-BZ0uKXK7.js
+++
@@ -1,9 +0,0 @@
-import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{r as u}from"./index-BKyFwriW.js";import{H as a}from"./HamburgerButton-DlOFlF2V.js";import"./_commonjsHelpers-CqkleIqs.js";import"./ThemeContext-EqqjHPk4.js";import"./Button-aJQ7c8Oe.js";import"./icons-B-89rpp_.js";import"./ButtonGroup-B9Za0ADD.js";const k={title:"Components/HamburgerButton",tags:["autodocs"],component:a,decorators:r=>t.jsx("div",{className:"storybookContainer",children:t.jsx(r,{})}),argTypes:{children:{if:{arg:"false",eq:"true"}},color:{control:{type:"select"}},variant:{control:{type:"inline-radio"}},ghostHover:{control:{type:"inline-radio"},if:{arg:"variant",eq:"ghost"}},size:{control:{type:"inline-radio"}},radius:{control:{type:"inline-radio"}},loadingPosition:{control:{type:"inline-radio"}},startIcon:{if:{arg:"false",exists:!0}},endIcon:{if:{arg:"false",exists:!0}},ref:{if:{arg:"false",exists:!0}},as:{if:{arg:"false",exists:!0}},wrapperClasses:{if:{arg:"false",exists:!0}}}},e={render:r=>{const[i,p]=u.useState(!1),l=()=>{p(c=>!c)};return t.jsx(a,{...r,isOpen:i,onClick:l})}};var s,o,n;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  render: props => {
-    const [isOpen, setIsOpen] = useState(false);
-    const clickHandler = () => {
-      setIsOpen(prevState => !prevState);
-    };
-    return <HamburgerButton {...props} isOpen={isOpen} onClick={clickHandler} />;
-  }
-}`,...(n=(o=e.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const v=["Default"];export{e as Default,v as __namedExportsOrder,k as default};
