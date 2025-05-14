caaf14ca16361083e0d1025f1a4a0a8b6ad04433
barrelrolla <chetkara@gmail.com>
2025-05-14 23:57:18 +0300

    added new icons

--- lib/icons/icons.tsx
+++ lib/icons/icons.tsx
@@ -199,6 +199,50 @@ export function RocketIcon({
   );
 }
 
+export function CoffeeIcon({
+  strokeWidth = defaultStrokeWidth,
+  ...rest
+}: SVGIconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 256 256"
+      {...rest}
+    >
+      <path
+        fill="currentColor"
+        d="M84 56V24a4 4 0 0 1 8 0v32a4 4 0 0 1-8 0m36 4a4 4 0 0 0 4-4V24a4 4 0 0 0-8 0v32a4 4 0 0 0 4 4m32 0a4 4 0 0 0 4-4V24a4 4 0 0 0-8 0v32a4 4 0 0 0 4 4m92 60v8a36 36 0 0 1-36 36h-.41a92.53 92.53 0 0 1-35.76 48H208a4 4 0 0 1 0 8H32a4 4 0 0 1 0-8h36.17A92.34 92.34 0 0 1 28 136V88a4 4 0 0 1 4-4h176a36 36 0 0 1 36 36m-40 16V92H36v44a84.28 84.28 0 0 0 48.21 76h71.58A84.28 84.28 0 0 0 204 136m32-16a28 28 0 0 0-24-27.71V136a91.8 91.8 0 0 1-2.2 19.94A28 28 0 0 0 236 128Z"
+        strokeWidth={strokeWidth}
+        stroke="currentColor"
+      ></path>
+    </svg>
+  );
+}
+
+export function CaretIcon({
+  strokeWidth = defaultStrokeWidth,
+  ...rest
+}: SVGIconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 256 256"
+      {...rest}
+    >
+      <path
+        fill="currentColor"
+        d="m210.83 98.83l-80 80a4 4 0 0 1-5.66 0l-80-80a4 4 0 0 1 5.66-5.66L128 170.34l77.17-77.17a4 4 0 1 1 5.66 5.66"
+        strokeWidth={strokeWidth}
+        stroke="currentColor"
+      ></path>
+    </svg>
+  );
+}
+
 export function GitHubIcon({
   strokeWidth = defaultStrokeWidth,
   ...rest
@@ -221,7 +265,7 @@ export function GitHubIcon({
   );
 }
 
-export function LinkedInLogo({
+export function LinkedInIcon({
   strokeWidth = defaultStrokeWidth,
   ...rest
 }: SVGIconProps) {
