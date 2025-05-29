3a0eb98d355fc1c714c6597dffb97bd3bc49d993
barrelrolla <chetkara@gmail.com>
2025-05-29 03:33:28 +0300

    Updates

---
+++ assets/Intro.stories-Dv3wpopb.js
@@ -0,0 +1,19 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./Anchor-US6rZC-y.js";import{B as t}from"./Button-Dtvs2u5Y.js";import"./ButtonGroup-tc9g7Ma_.js";import"./CardSection-CiLcWNQW.js";import"./DarkModeToggle-B03q3AiI.js";import"./DarkModeSelector-D3tdDb_C.js";import"./Divider-C9_WKfr7.js";import"./FooterLinksTitle-BXYn9y9u.js";import"./HamburgerButton--Xwj-67v.js";import{e as i,b as a,a as m,d as c}from"./HeroText-CbxtFJAS.js";import"./InputField-YhfZm_bc.js";import"./NavbarToggle-DlNPSSRD.js";import"./bundle-mjs-CFgYV38x.js";import"./icons-D2S3JZ5k.js";import"./index-BKyFwriW.js";import"./_commonjsHelpers-CqkleIqs.js";const S={title:"Intro/Welcome"},o={render:()=>r.jsx("div",{className:"bg-main",children:r.jsx(i,{textAlign:"center",className:"h-screen min-h-0",children:r.jsxs(a,{children:[r.jsx(m,{children:"Welcome to my react components library"}),r.jsxs(c,{children:[r.jsx(t,{as:"a",href:"./?path=/story/components",variant:"outline",children:"Components"}),r.jsx(t,{as:"a",href:"./?path=/docs/intro-installation--docs",children:"Installation"})]})]})})})};var e,n,s;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: () => {
+    return <div className="bg-main">\r
+        <Hero textAlign="center" className="h-screen min-h-0">\r
+          <HeroSection>\r
+            <HeroTitle>Welcome to my react components library</HeroTitle>\r
+            <HeroActions>\r
+              <Button as="a" href="./?path=/story/components" variant="outline">\r
+                Components\r
+              </Button>\r
+              <Button as="a" href="./?path=/docs/intro-installation--docs">\r
+                Installation\r
+              </Button>\r
+            </HeroActions>\r
+          </HeroSection>\r
+        </Hero>\r
+      </div>;
+  }
+}`,...(s=(n=o.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const I=["Welcome"];export{o as Welcome,I as __namedExportsOrder,S as default};
