463e0dadeb4f66a1c1a595a2fe9f122bcf67209f
Julian <chetkara@gmail.com>
2026-06-30 17:22:40 +0300

    Updates

---
+++ assets/Card.stories-BQPF0V0o.js
@@ -0,0 +1,148 @@
+import{j as r}from"./iframe-DG_BMKwG.js";import{B as o}from"./Button-BHGnnOCe.js";import"./ButtonGroup-Cz86zFeM.js";import{C as a,a as n,b as s,c as d,d as c,e as i,f as t}from"./CardTitle-CRt3Fk_V.js";import"./preload-helper-Dp1pzeXC.js";import"./icons-_aW9hqBU.js";import"./bundle-mjs-169JcMJR.js";import"./Divider-BQedFSrJ.js";const M={title:"Components/Card",tags:["autodocs"],component:t,subcomponents:{CardActions:i,CardInteract:c,CardImageContainer:d,CardTitle:s,CardText:n,CardSection:a},decorators:e=>r.jsx("div",{className:"storybookContainer",children:r.jsx(e,{})}),argTypes:{size:{control:{type:"inline-radio"}}}},l={render:({...e})=>r.jsxs(t,{...e,children:[r.jsx(s,{children:"Lorem, ipsum dolor."}),r.jsx(n,{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, id!"}),r.jsx(i,{children:r.jsx(o,{children:"Learn more"})})]})},m={render:({...e})=>r.jsx(t,{...e,children:r.jsxs(c,{href:"#",children:[r.jsx(s,{children:"Lorem, ipsum dolor."}),r.jsx(n,{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, id!"})]})})},p={render:({...e})=>r.jsxs(t,{...e,children:[r.jsx(a,{className:"flex-1/3",children:r.jsx(d,{children:r.jsx("img",{className:"card-image",src:"https://picsum.photos/400/200",alt:"placeholder image"})})}),r.jsxs(a,{className:"flex-2/3",children:[r.jsx(s,{children:"Lorem, ipsum dolor."}),r.jsx(n,{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, id!"}),r.jsx(i,{children:r.jsx(o,{children:"Learn more"})})]})]})},C={render:({...e})=>r.jsxs(t,{...e,children:[r.jsx(a,{className:"flex-1/3",children:r.jsx(c,{href:"#",children:r.jsx(d,{children:r.jsx("img",{className:"card-image",src:"https://picsum.photos/400/200",alt:"placeholder image"})})})}),r.jsxs(a,{className:"flex-2/3",children:[r.jsx(s,{children:"Lorem, ipsum dolor."}),r.jsx(n,{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, id!"}),r.jsx(i,{children:r.jsx(o,{children:"Learn more"})})]})]})},u={render:({...e})=>r.jsxs(t,{...e,children:[r.jsxs(c,{href:"#",children:[r.jsx(a,{children:r.jsx(d,{children:r.jsx("img",{className:"card-image",src:"https://picsum.photos/400/200",alt:"placeholder image"})})}),r.jsxs(a,{children:[r.jsx(s,{children:"Lorem, ipsum dolor."}),r.jsx(n,{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, id!"})]})]}),r.jsx(i,{children:r.jsx(o,{children:"Learn more"})})]})},h={render:({...e})=>r.jsxs(t,{...e,children:[r.jsx(a,{className:"flex-1/3",children:r.jsx(c,{href:"#",children:r.jsx(d,{padded:!0,children:r.jsx("img",{className:"card-image",src:"https://picsum.photos/400/200",alt:"placeholder image"})})})}),r.jsxs(a,{className:"flex-2/3",children:[r.jsx(s,{children:"Lorem, ipsum dolor."}),r.jsx(n,{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, id!"}),r.jsx(i,{children:r.jsx(o,{children:"Learn more"})})]})]})},x={render:({size:e="lg",..._})=>r.jsxs(t,{horizontal:!0,size:e,..._,children:[r.jsx(a,{className:"flex-1/3",children:r.jsx(d,{children:r.jsx("img",{className:"card-image",src:"https://picsum.photos/400/200",alt:"placeholder image"})})}),r.jsxs(a,{className:"flex-2/3",children:[r.jsx(s,{children:"Lorem, ipsum dolor."}),r.jsx(n,{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, id!"}),r.jsx(i,{children:r.jsx(o,{children:"Learn more"})})]})]}),argTypes:{horizontal:{if:{arg:"false",eq:"true"}}}};var g,j,L;l.parameters={...l.parameters,docs:{...(g=l.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: ({
+    ...rest
+  }) => {
+    return <Card {...rest}>\r
+        <CardTitle>Lorem, ipsum dolor.</CardTitle>\r
+        <CardText>\r
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, id!\r
+        </CardText>\r
+        <CardActions>\r
+          <Button>Learn more</Button>\r
+        </CardActions>\r
+      </Card>;
+  }
+}`,...(L=(j=l.parameters)==null?void 0:j.docs)==null?void 0:L.source}}};var f,T,S;m.parameters={...m.parameters,docs:{...(f=m.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: ({
+    ...rest
+  }) => {
+    return <Card {...rest}>\r
+        <CardInteract href="#">\r
+          <CardTitle>Lorem, ipsum dolor.</CardTitle>\r
+          <CardText>\r
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, id!\r
+          </CardText>\r
+        </CardInteract>\r
+      </Card>;
+  }
+}`,...(S=(T=m.parameters)==null?void 0:T.docs)==null?void 0:S.source}}};var N,I,B;p.parameters={...p.parameters,docs:{...(N=p.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  render: ({
+    ...rest
+  }) => {
+    return <Card {...rest}>\r
+        <CardSection className="flex-1/3">\r
+          <CardImageContainer>\r
+            <img className="card-image" src="https://picsum.photos/400/200" alt="placeholder image"></img>\r
+          </CardImageContainer>\r
+        </CardSection>\r
+        <CardSection className="flex-2/3">\r
+          <CardTitle>Lorem, ipsum dolor.</CardTitle>\r
+          <CardText>\r
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, id!\r
+          </CardText>\r
+          <CardActions>\r
+            <Button>Learn more</Button>\r
+          </CardActions>\r
+        </CardSection>\r
+      </Card>;
+  }
+}`,...(B=(I=p.parameters)==null?void 0:I.docs)==null?void 0:B.source}}};var V,A,z;C.parameters={...C.parameters,docs:{...(V=C.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  render: ({
+    ...rest
+  }) => {
+    return <Card {...rest}>\r
+        <CardSection className="flex-1/3">\r
+          <CardInteract href="#">\r
+            <CardImageContainer>\r
+              <img className="card-image" src="https://picsum.photos/400/200" alt="placeholder image"></img>\r
+            </CardImageContainer>\r
+          </CardInteract>\r
+        </CardSection>\r
+        <CardSection className="flex-2/3">\r
+          <CardTitle>Lorem, ipsum dolor.</CardTitle>\r
+          <CardText>\r
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, id!\r
+          </CardText>\r
+          <CardActions>\r
+            <Button>Learn more</Button>\r
+          </CardActions>\r
+        </CardSection>\r
+      </Card>;
+  }
+}`,...(z=(A=C.parameters)==null?void 0:A.docs)==null?void 0:z.source}}};var b,W,k;u.parameters={...u.parameters,docs:{...(b=u.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: ({
+    ...rest
+  }) => {
+    return <Card {...rest}>\r
+        <CardInteract href="#">\r
+          <CardSection>\r
+            <CardImageContainer>\r
+              <img className="card-image" src="https://picsum.photos/400/200" alt="placeholder image"></img>\r
+            </CardImageContainer>\r
+          </CardSection>\r
+          <CardSection>\r
+            <CardTitle>Lorem, ipsum dolor.</CardTitle>\r
+            <CardText>\r
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, id!\r
+            </CardText>\r
+          </CardSection>\r
+        </CardInteract>\r
+        <CardActions>\r
+          <Button>Learn more</Button>\r
+        </CardActions>\r
+      </Card>;
+  }
+}`,...(k=(W=u.parameters)==null?void 0:W.docs)==null?void 0:k.source}}};var y,q,D;h.parameters={...h.parameters,docs:{...(y=h.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: ({
+    ...rest
+  }) => {
+    return <Card {...rest}>\r
+        <CardSection className="flex-1/3">\r
+          <CardInteract href="#">\r
+            <CardImageContainer padded>\r
+              <img className="card-image" src="https://picsum.photos/400/200" alt="placeholder image"></img>\r
+            </CardImageContainer>\r
+          </CardInteract>\r
+        </CardSection>\r
+        <CardSection className="flex-2/3">\r
+          <CardTitle>Lorem, ipsum dolor.</CardTitle>\r
+          <CardText>\r
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, id!\r
+          </CardText>\r
+          <CardActions>\r
+            <Button>Learn more</Button>\r
+          </CardActions>\r
+        </CardSection>\r
+      </Card>;
+  }
+}`,...(D=(q=h.parameters)==null?void 0:q.docs)==null?void 0:D.source}}};var E,H,P;x.parameters={...x.parameters,docs:{...(E=x.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  render: ({
+    size = "lg",
+    ...rest
+  }) => {
+    return <Card horizontal size={size} {...rest}>\r
+        <CardSection className="flex-1/3">\r
+          <CardImageContainer>\r
+            <img className="card-image" src="https://picsum.photos/400/200" alt="placeholder image"></img>\r
+          </CardImageContainer>\r
+        </CardSection>\r
+        <CardSection className="flex-2/3">\r
+          <CardTitle>Lorem, ipsum dolor.</CardTitle>\r
+          <CardText>\r
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, id!\r
+          </CardText>\r
+          <CardActions>\r
+            <Button>Learn more</Button>\r
+          </CardActions>\r
+        </CardSection>\r
+      </Card>;
+  },
+  argTypes: {
+    horizontal: {
+      if: {
+        arg: "false",
+        eq: "true"
+      }
+    }
+  }
+}`,...(P=(H=x.parameters)==null?void 0:H.docs)==null?void 0:P.source}}};const Q=["Default","Clickable","WithImage","WithClickableImage","WithClickableBody","WithPaddedImage","Horizontal"];export{m as Clickable,l as Default,x as Horizontal,u as WithClickableBody,C as WithClickableImage,p as WithImage,h as WithPaddedImage,Q as __namedExportsOrder,M as default};
---
+++ assets/Intro.stories-DemzXlDg.js
@@ -0,0 +1,19 @@
+import{j as r}from"./iframe-DG_BMKwG.js";import"./Anchor-DLLkgwOQ.js";import"./Badge-67TRsyaF.js";import{B as t}from"./Button-BHGnnOCe.js";import"./ButtonGroup-Cz86zFeM.js";import"./CardTitle-CRt3Fk_V.js";import"./DarkModeToggle-PkjU3t0p.js";import"./DarkModeSelector-BCYoLGpm.js";import"./Divider-BQedFSrJ.js";import"./FooterLinksTitle-iCqIUETT.js";import"./HamburgerButton-CS-TR7YF.js";import{H as i,a,b as m,c}from"./HeroText-BDd2kJ9z.js";import"./Input-B1HZxZ67.js";import"./NavbarToggle-BXlFnk0u.js";import"./preload-helper-Dp1pzeXC.js";import"./bundle-mjs-169JcMJR.js";import"./icons-_aW9hqBU.js";const S={title:"Intro/Welcome"},o={render:()=>r.jsx("div",{className:"bg-main",children:r.jsx(i,{textAlign:"center",className:"h-screen min-h-0",children:r.jsxs(a,{children:[r.jsx(m,{children:"Welcome to my react components library"}),r.jsxs(c,{children:[r.jsx(t,{as:"a",href:"./?path=/story/components",variant:"outline",children:"Components"}),r.jsx(t,{as:"a",href:"./?path=/docs/intro-installation--docs",children:"Installation"})]})]})})})};var e,n,s;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: () => {
+    return <div className="bg-main">\r
+        <Hero textAlign="center" className="h-screen min-h-0">\r
+          <HeroSection>\r
+            <HeroTitle>Welcome to my react components library</HeroTitle>\r
+            <HeroActions>\r
+              <Button as="a" href="./?path=/story/components" variant="outline">\r
+                Components\r
+              </Button>\r
+              <Button as="a" href="./?path=/docs/intro-installation--docs">\r
+                Installation\r
+              </Button>\r
+            </HeroActions>\r
+          </HeroSection>\r
+        </Hero>\r
+      </div>;
+  }
+}`,...(s=(n=o.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const I=["Welcome"];export{o as Welcome,I as __namedExportsOrder,S as default};
