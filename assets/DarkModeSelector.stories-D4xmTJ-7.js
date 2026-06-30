41a8843dd47539f56c0b5d720656c598256bacd6
Julian <chetkara@gmail.com>
2026-06-30 18:42:58 +0300

    Updates

--- assets/DarkModeSelector.stories-D4xmTJ-7.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-DG_BMKwG.js";import{T as i}from"./Button-BHGnnOCe.js";import{D as a}from"./DarkModeSelector-BCYoLGpm.js";import"./preload-helper-Dp1pzeXC.js";import"./icons-_aW9hqBU.js";import"./bundle-mjs-169JcMJR.js";import"./ButtonGroup-Cz86zFeM.js";import"./Divider-BQedFSrJ.js";const h={title:"Components/DarkModeSelector",tags:["autodocs"],component:a,decorators:o=>e.jsx("div",{className:"storybookContainer",children:e.jsx(o,{})}),argTypes:{color:{control:{type:"select"}},variant:{control:{type:"inline-radio"}},size:{control:{type:"inline-radio"}},radius:{control:{type:"inline-radio"}},ghostHover:{control:{type:"inline-radio"},if:{arg:"variant",eq:"ghost"}},children:{if:{arg:"false",exists:!0}}}},r={render:({...o})=>e.jsx(i,{children:e.jsx(a,{...o})})};var t,n,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: ({
-    ...props
-  }) => {
-    return <ThemeContextProvider>\r
-        <DarkModeSelector {...props} />\r
-      </ThemeContextProvider>;
-  }
-}`,...(s=(n=r.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,h as default};
