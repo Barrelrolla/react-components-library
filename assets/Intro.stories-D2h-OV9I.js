869e2542f9a6238a142fc9af42b00f4fe3dbf1e5
Julian <chetkara@gmail.com>
2026-07-07 01:50:21 +0300

    Updates

---
+++ assets/Intro.stories-D2h-OV9I.js
@@ -0,0 +1,19 @@
+import{j as r}from"./iframe-MBsWu6_L.js";import"./Anchor-90eROSMV.js";import"./Badge-dsdWEG9b.js";import{B as t}from"./Button-BaZVx-ha.js";import"./ButtonGroup-BbmRHpPX.js";import"./CardTitle-aWu6zyR8.js";import"./DarkModeToggle-C58mr16S.js";import"./DarkModeSelector-DtDAi9Uc.js";import"./Divider-Bl-SgyaY.js";import"./FooterLinksTitle-CdyDIg34.js";import"./HamburgerButton-oHmt4I8d.js";import{H as i,a,b as m,c}from"./HeroText-By9tbdKd.js";import"./Input-B96JsDwY.js";import"./NavbarToggle-D8uqwsom.js";import"./preload-helper-Dp1pzeXC.js";import"./bundle-mjs-169JcMJR.js";import"./icons-DIUpUHpZ.js";const S={title:"Intro/Welcome"},o={render:()=>r.jsx("div",{className:"bg-main",children:r.jsx(i,{textAlign:"center",className:"h-screen min-h-0",children:r.jsxs(a,{children:[r.jsx(m,{children:"Welcome to my react components library"}),r.jsxs(c,{children:[r.jsx(t,{as:"a",href:"./?path=/story/components",variant:"outline",children:"Components"}),r.jsx(t,{as:"a",href:"./?path=/docs/intro-installation--docs",children:"Installation"})]})]})})})};var e,n,s;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: () => {
+    return <div className="bg-main">\r
+        <Hero textAlign="center" className="h-screen min-h-0">\r
+          <HeroSection>\r
+            <HeroTitle>Welcome to my react components library</HeroTitle>\r
+            <HeroActions>\r
+              <Button as="a" href="./?path=/story/components" variant="outline">\r
+                Components\r
+              </Button>\r
+              <Button as="a" href="./?path=/docs/intro-installation--docs">\r
+                Installation\r
+              </Button>\r
+            </HeroActions>\r
+          </HeroSection>\r
+        </Hero>\r
+      </div>;
+  }
+}`,...(s=(n=o.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const I=["Welcome"];export{o as Welcome,I as __namedExportsOrder,S as default};
