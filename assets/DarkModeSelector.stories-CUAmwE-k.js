81c61424c7ed8e656b79dd46bc020edf01663172
Julian <chetkara@gmail.com>
2026-07-07 11:16:35 +0300

    Updates

--- assets/DarkModeSelector.stories-CUAmwE-k.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-DcjsNGY-.js";import{T as i}from"./Button-BJja0PQU.js";import{D as a}from"./DarkModeSelector-CsgonWgz.js";import"./preload-helper-Dp1pzeXC.js";import"./icons-D2vjWccA.js";import"./bundle-mjs-169JcMJR.js";import"./ButtonGroup-4nW1dL7x.js";import"./Divider-DomAVFPU.js";const h={title:"Components/DarkModeSelector",tags:["autodocs"],component:a,decorators:o=>e.jsx("div",{className:"storybookContainer",children:e.jsx(o,{})}),argTypes:{color:{control:{type:"select"}},variant:{control:{type:"inline-radio"}},size:{control:{type:"inline-radio"}},radius:{control:{type:"inline-radio"}},ghostHover:{control:{type:"inline-radio"},if:{arg:"variant",eq:"ghost"}},children:{if:{arg:"false",exists:!0}}}},r={render:({...o})=>e.jsx(i,{children:e.jsx(a,{...o})})};var t,n,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: ({
-    ...props
-  }) => {
-    return <ThemeContextProvider>\r
-        <DarkModeSelector {...props} />\r
-      </ThemeContextProvider>;
-  }
-}`,...(s=(n=r.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,h as default};
---
+++ assets/DarkModeSelector.stories-Ccy3hJBp.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-CY_PJYz6.js";import{T as i}from"./Button-B1WwNQEI.js";import{D as a}from"./DarkModeSelector-DO2V3Pjy.js";import"./preload-helper-Dp1pzeXC.js";import"./icons-UuyvQICh.js";import"./bundle-mjs-169JcMJR.js";import"./ButtonGroup-D8HLGvVf.js";import"./Divider-BWMB6Qnz.js";const h={title:"Components/DarkModeSelector",tags:["autodocs"],component:a,decorators:o=>e.jsx("div",{className:"storybookContainer",children:e.jsx(o,{})}),argTypes:{color:{control:{type:"select"}},variant:{control:{type:"inline-radio"}},size:{control:{type:"inline-radio"}},radius:{control:{type:"inline-radio"}},ghostHover:{control:{type:"inline-radio"},if:{arg:"variant",eq:"ghost"}},children:{if:{arg:"false",exists:!0}}}},r={render:({...o})=>e.jsx(i,{children:e.jsx(a,{...o})})};var t,n,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: ({
+    ...props
+  }) => {
+    return <ThemeContextProvider>\r
+        <DarkModeSelector {...props} />\r
+      </ThemeContextProvider>;
+  }
+}`,...(s=(n=r.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,h as default};
