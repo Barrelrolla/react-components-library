45ecba4501bfc9fec6b779d0818c9f65251fc18a
Julian <chetkara@gmail.com>
2026-07-07 11:04:59 +0300

    Updates

--- assets/DarkModeSelector.stories-Ol0n4F7H.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-MBsWu6_L.js";import{T as i}from"./Button-BaZVx-ha.js";import{D as a}from"./DarkModeSelector-DtDAi9Uc.js";import"./preload-helper-Dp1pzeXC.js";import"./icons-DIUpUHpZ.js";import"./bundle-mjs-169JcMJR.js";import"./ButtonGroup-BbmRHpPX.js";import"./Divider-Bl-SgyaY.js";const h={title:"Components/DarkModeSelector",tags:["autodocs"],component:a,decorators:o=>e.jsx("div",{className:"storybookContainer",children:e.jsx(o,{})}),argTypes:{color:{control:{type:"select"}},variant:{control:{type:"inline-radio"}},size:{control:{type:"inline-radio"}},radius:{control:{type:"inline-radio"}},ghostHover:{control:{type:"inline-radio"},if:{arg:"variant",eq:"ghost"}},children:{if:{arg:"false",exists:!0}}}},r={render:({...o})=>e.jsx(i,{children:e.jsx(a,{...o})})};var t,n,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: ({
-    ...props
-  }) => {
-    return <ThemeContextProvider>\r
-        <DarkModeSelector {...props} />\r
-      </ThemeContextProvider>;
-  }
-}`,...(s=(n=r.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,h as default};
---
+++ assets/Intro.stories-DvgJoliY.js
@@ -0,0 +1,19 @@
+import{j as r}from"./iframe-DcjsNGY-.js";import"./Anchor-B16w7Xhr.js";import"./Badge-bnu2SBh_.js";import{B as t}from"./Button-BJja0PQU.js";import"./ButtonGroup-4nW1dL7x.js";import"./CardTitle-DH5erhnR.js";import"./DarkModeToggle--etSyF5L.js";import"./DarkModeSelector-CsgonWgz.js";import"./Divider-DomAVFPU.js";import"./FooterLinksTitle-CdOPDZn2.js";import"./HamburgerButton-DvwYBV-J.js";import{H as i,a,b as m,c}from"./HeroText-DQXVThEV.js";import"./Input-BpPzmg5o.js";import"./NavbarToggle-CxCdvhiD.js";import"./preload-helper-Dp1pzeXC.js";import"./bundle-mjs-169JcMJR.js";import"./icons-D2vjWccA.js";const S={title:"Intro/Welcome"},o={render:()=>r.jsx("div",{className:"bg-main",children:r.jsx(i,{textAlign:"center",className:"h-screen min-h-0",children:r.jsxs(a,{children:[r.jsx(m,{children:"Welcome to my react components library"}),r.jsxs(c,{children:[r.jsx(t,{as:"a",href:"./?path=/story/components",variant:"outline",children:"Components"}),r.jsx(t,{as:"a",href:"./?path=/docs/intro-installation--docs",children:"Installation"})]})]})})})};var e,n,s;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: () => {
+    return <div className="bg-main">\r
+        <Hero textAlign="center" className="h-screen min-h-0">\r
+          <HeroSection>\r
+            <HeroTitle>Welcome to my react components library</HeroTitle>\r
+            <HeroActions>\r
+              <Button as="a" href="./?path=/story/components" variant="outline">\r
+                Components\r
+              </Button>\r
+              <Button as="a" href="./?path=/docs/intro-installation--docs">\r
+                Installation\r
+              </Button>\r
+            </HeroActions>\r
+          </HeroSection>\r
+        </Hero>\r
+      </div>;
+  }
+}`,...(s=(n=o.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const I=["Welcome"];export{o as Welcome,I as __namedExportsOrder,S as default};
