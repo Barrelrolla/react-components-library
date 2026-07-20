fab6ab7d1b50ab25debb9917baaab213fe3346e1
Julian <chetkara@gmail.com>
2026-07-21 00:08:26 +0300

    Updates

---
+++ assets/Hero.stories-DIRhMEL8.js
@@ -0,0 +1,46 @@
+import{j as e}from"./iframe-YYkmocuX.js";import{d as s,b as n,a,e as H,c as m,H as c}from"./HeroText-CUhQOahC.js";import{B as i}from"./Button-DxzP1f3Y.js";import"./ButtonGroup-DhtqODt8.js";import{c as g,a as j}from"./index-DnWjc-IH.js";import"./preload-helper-Dp1pzeXC.js";import"./bundle-mjs-169JcMJR.js";import"./Divider-D4EhF-0V.js";const A={title:"Components/Hero",tags:["autodocs"],component:c,subcomponents:{HeroActions:m,HeroImageSection:H,HeroSection:a,HeroTitle:n,HeroText:s},argTypes:{children:{table:{disable:!0}},color:{control:{type:"select"},options:j},textAlign:{control:{type:"inline-radio"}},actionsAlign:{control:{type:"inline-radio"}},responsiveAt:{control:{type:"inline-radio"},options:g},wrapperClasses:{table:{disable:!0}},wrapperStyle:{table:{disable:!0}}}},r={render:({...t})=>e.jsx("div",{className:"bg-main",children:e.jsx(c,{...t,children:e.jsxs(a,{children:[e.jsx(n,{children:"Lorem ipsum dolor sit amet."}),e.jsx(s,{children:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore ex, corporis rem dolores culpa quo corrupti doloremque minima excepturi molestiae."}),e.jsxs(m,{children:[e.jsx(i,{children:"action"}),e.jsx(i,{children:"action"})]})]})})})},o={render:({...t})=>e.jsx("div",{className:"bg-main",children:e.jsxs(c,{...t,children:[e.jsxs(a,{className:"max-md:h-1/2 md:w-1/2",children:[e.jsx(n,{children:"Lorem ipsum dolor sit amet."}),e.jsx(s,{children:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore ex, corporis rem dolores culpa quo corrupti doloremque minima excepturi molestiae."}),e.jsxs(m,{children:[e.jsx(i,{children:"action"}),e.jsx(i,{children:"action"})]})]}),e.jsx(H,{className:"max-md:h-1/2 md:w-1/2",children:e.jsx("img",{className:"hero-image md:w-[50vw]",src:"https://picsum.photos/1400/1400",alt:"placeholder image"})})]})})};var l,p,d;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: ({
+    ...props
+  }) => {
+    return <div className="bg-main">\r
+        <Hero {...props}>\r
+          <HeroSection>\r
+            <HeroTitle>Lorem ipsum dolor sit amet.</HeroTitle>\r
+            <HeroText>\r
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore\r
+              ex, corporis rem dolores culpa quo corrupti doloremque minima\r
+              excepturi molestiae.\r
+            </HeroText>\r
+            <HeroActions>\r
+              <Button>action</Button>\r
+              <Button>action</Button>\r
+            </HeroActions>\r
+          </HeroSection>\r
+        </Hero>\r
+      </div>;
+  }
+}`,...(d=(p=r.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};var u,x,h;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: ({
+    ...props
+  }) => {
+    return <div className="bg-main">\r
+        <Hero {...props}>\r
+          <HeroSection className="max-md:h-1/2 md:w-1/2">\r
+            <HeroTitle>Lorem ipsum dolor sit amet.</HeroTitle>\r
+            <HeroText>\r
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore\r
+              ex, corporis rem dolores culpa quo corrupti doloremque minima\r
+              excepturi molestiae.\r
+            </HeroText>\r
+            <HeroActions>\r
+              <Button>action</Button>\r
+              <Button>action</Button>\r
+            </HeroActions>\r
+          </HeroSection>\r
+          <HeroImageSection className="max-md:h-1/2 md:w-1/2">\r
+            <img className="hero-image md:w-[50vw]" src="https://picsum.photos/1400/1400" alt="placeholder image" />\r
+          </HeroImageSection>\r
+        </Hero>\r
+      </div>;
+  }
+}`,...(h=(x=o.parameters)==null?void 0:x.docs)==null?void 0:h.source}}};const L=["Default","WithImage"];export{r as Default,o as WithImage,L as __namedExportsOrder,A as default};
