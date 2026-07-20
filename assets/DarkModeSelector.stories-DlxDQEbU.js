ec7effa9acd8c5a235d007b46c8e989540534a93
Julian <chetkara@gmail.com>
2026-07-20 21:57:02 +0300

    Updates

---
+++ assets/DarkModeSelector.stories-DlxDQEbU.js
@@ -0,0 +1,9 @@
+import{j as r}from"./iframe-DihAyM6x.js";import{T as n}from"./Button-wfMEksPL.js";import{D as i}from"./DarkModeSelector-CL2qOCm-.js";import{b as l,a as d}from"./index-DnWjc-IH.js";import"./preload-helper-Dp1pzeXC.js";import"./bundle-mjs-169JcMJR.js";import"./ButtonGroup-BGgP2ctq.js";import"./Divider-Cn1xnO3G.js";const h={title:"Components/DarkModeSelector",tags:["autodocs"],component:i,decorators:o=>r.jsx("div",{className:"storybookContainer",children:r.jsx(o,{})}),argTypes:{color:{control:{type:"select"},options:d},variant:{control:{type:"inline-radio"}},size:{control:{type:"inline-radio"},options:l},radius:{control:{type:"inline-radio"}},ghostHover:{control:{type:"inline-radio"},if:{arg:"variant",eq:"ghost"}},dividerClasses:{table:{disable:!0}},wrapperClasses:{table:{disable:!0}},className:{table:{disable:!0}},children:{table:{disable:!0}}}},e={render:({...o})=>r.jsx(n,{children:r.jsx(i,{...o})})};var t,a,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: ({
+    ...props
+  }) => {
+    return <ThemeContextProvider>\r
+        <DarkModeSelector {...props} />\r
+      </ThemeContextProvider>;
+  }
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const C=["Default"];export{e as Default,C as __namedExportsOrder,h as default};
---
+++ assets/DarkModeToggle.stories-CkTDF58d.js
@@ -0,0 +1,9 @@
+import{j as r}from"./iframe-DihAyM6x.js";import{T as n}from"./Button-wfMEksPL.js";import{D as i}from"./DarkModeToggle-CGGgD83r.js";import{b as l,a as d}from"./index-DnWjc-IH.js";import"./preload-helper-Dp1pzeXC.js";import"./bundle-mjs-169JcMJR.js";import"./ButtonGroup-BGgP2ctq.js";import"./Divider-Cn1xnO3G.js";const v={title:"Components/DarkModeToggle",tags:["autodocs"],component:i,decorators:o=>r.jsx("div",{className:"storybookContainer",children:r.jsx(o,{})}),argTypes:{children:{if:{arg:"false",eq:"true"}},color:{control:{type:"select"},options:d},variant:{control:{type:"inline-radio"}},ghostHover:{control:{type:"inline-radio"},if:{arg:"variant",eq:"ghost"}},size:{control:{type:"inline-radio"},options:l},radius:{control:{type:"inline-radio"}},loading:{table:{disable:!0}},loadingPosition:{table:{disable:!0}},startIcon:{table:{disable:!0}},endIcon:{table:{disable:!0}},ref:{table:{disable:!0}},as:{table:{disable:!0}},wrapperClasses:{table:{disable:!0}}}},e={render:({...o})=>r.jsx(n,{children:r.jsx(i,{...o})})};var t,a,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: ({
+    ...props
+  }) => {
+    return <ThemeContextProvider>\r
+        <DarkModeToggle {...props} />\r
+      </ThemeContextProvider>;
+  }
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const h=["Default"];export{e as Default,h as __namedExportsOrder,v as default};
