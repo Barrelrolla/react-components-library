b08b335869547136e1929a4ad5cec39dd15932dd
Julian <chetkara@gmail.com>
2026-07-21 22:27:16 +0300

    fixed radio buttons outline radius

--- lib/plugin/components/radio/index.ts
+++ lib/plugin/components/radio/index.ts
@@ -2,7 +2,7 @@ import { CssInJs } from "@/types";
 
 export default <Record<string, CssInJs>>{
   ".radio": {
-    "@apply pointer-events-none absolute h-(--size) w-(--size) text-(--fg-color) peer-hover:fg-hover peer-active:fg-active peer-focus-visible:outline-2 -outline-offset-1 rounded-sm transition-all":
+    "@apply pointer-events-none absolute h-(--size) w-(--size) text-(--fg-color) peer-hover:fg-hover peer-active:fg-active peer-focus-visible:outline-2 outline-offset-0 rounded-full transition-all":
       {},
   },
   ".radio-wrapper": {
