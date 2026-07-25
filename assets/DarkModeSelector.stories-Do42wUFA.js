cbe08ab58408192562ca1650206acd1b670c2013
Julian <chetkara@gmail.com>
2026-07-25 12:55:12 +0300

    Updates

--- assets/DarkModeSelector.stories-Do42wUFA.js
+++
@@ -1,9 +0,0 @@
-import{j as r}from"./iframe-CWDiRE6U.js";import{T as a}from"./Button-BV3-QB9s.js";import{D as t}from"./DarkModeSelector-CopLgaEd.js";import{b as s,a as i}from"./index-Ccfv0_tG.js";import"./preload-helper-PPVm8Dsz.js";import"./icons-cedDc1Nc.js";import"./bundle-mjs-169JcMJR.js";import"./ButtonGroup-DutiQ9nS.js";import"./Divider-DaC1_Fb4.js";import"./floating-ui.react-CSRjdzjT.js";import"./TooltipContent-BDJzj8I4.js";import"./Slot-CH3rGvE7.js";const C={title:"Components/DarkModeSelector",tags:["autodocs"],component:t,decorators:o=>r.jsx("div",{className:"storybookContainer",children:r.jsx(o,{})}),argTypes:{color:{control:{type:"select"},options:i},variant:{control:{type:"inline-radio"}},size:{control:{type:"inline-radio"},options:s},radius:{control:{type:"inline-radio"}},ghostHover:{control:{type:"inline-radio"},if:{arg:"variant",eq:"ghost"}},dividerClasses:{table:{disable:!0}},wrapperClasses:{table:{disable:!0}},className:{table:{disable:!0}},children:{table:{disable:!0}}}},e={render:({...o})=>r.jsx(a,{children:r.jsx(t,{...o})})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  render: ({
-    ...props
-  }) => {
-    return <ThemeContextProvider>\r
-        <DarkModeSelector {...props} />\r
-      </ThemeContextProvider>;
-  }
-}`,...e.parameters?.docs?.source}}};const y=["Default"];export{e as Default,y as __namedExportsOrder,C as default};
---
+++ assets/DarkModeToggle.stories-NFyX1t-9.js
@@ -0,0 +1,9 @@
+import{j as r}from"./iframe-AHNl2UaN.js";import{T as a}from"./Button-Dsp9tKMZ.js";import{D as t}from"./DarkModeToggle-B9Rz79K-.js";import{b as i,a as s}from"./index-Ccfv0_tG.js";import"./preload-helper-PPVm8Dsz.js";import"./icons-DBHgraxs.js";import"./bundle-mjs-169JcMJR.js";import"./ButtonGroup-qmadaT91.js";import"./Divider-B5zrAL12.js";import"./floating-ui.react-BOKeVWY2.js";import"./TooltipContent-D5_8DcC2.js";import"./Slot-dw-BdoQo.js";const h={title:"Components/DarkModeToggle",tags:["autodocs"],component:t,decorators:o=>r.jsx("div",{className:"storybookContainer",children:r.jsx(o,{})}),argTypes:{children:{if:{arg:"false",eq:"true"}},color:{control:{type:"select"},options:s},variant:{control:{type:"inline-radio"}},ghostHover:{control:{type:"inline-radio"},if:{arg:"variant",eq:"ghost"}},size:{control:{type:"inline-radio"},options:i},radius:{control:{type:"inline-radio"}},loading:{table:{disable:!0}},loadingPosition:{table:{disable:!0}},startIcon:{table:{disable:!0}},endIcon:{table:{disable:!0}},ref:{table:{disable:!0}},as:{table:{disable:!0}},wrapperClasses:{table:{disable:!0}}}},e={render:({...o})=>r.jsx(a,{children:r.jsx(t,{...o})})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: ({
+    ...props
+  }) => {
+    return <ThemeContextProvider>\r
+        <DarkModeToggle {...props} />\r
+      </ThemeContextProvider>;
+  }
+}`,...e.parameters?.docs?.source}}};const T=["Default"];export{e as Default,T as __namedExportsOrder,h as default};
