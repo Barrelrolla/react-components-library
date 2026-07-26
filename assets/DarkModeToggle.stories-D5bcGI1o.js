12a6775a4e76c40a068c160001b0034c19f41bf5
Julian <chetkara@gmail.com>
2026-07-27 02:36:46 +0300

    Updates

--- assets/DarkModeToggle.stories-D5bcGI1o.js
+++
@@ -1,9 +0,0 @@
-import{j as r}from"./iframe-CjBjUzSl.js";import{T as a}from"./Button-DIxUeA0Q.js";import{D as t}from"./DarkModeToggle-D-fDp3B4.js";import{b as i,a as s}from"./index-DhdEVH7N.js";import"./preload-helper-PPVm8Dsz.js";import"./icons-J8SgOh6C.js";import"./bundle-mjs-169JcMJR.js";import"./ButtonGroup-uoyj0QY8.js";import"./Divider-BrPIuomX.js";import"./floating-ui.react-DS28FwR0.js";import"./TooltipContent-1CWEpyEG.js";import"./Slot-CmjfqFlE.js";const h={title:"Components/DarkModeToggle",tags:["autodocs"],component:t,decorators:o=>r.jsx("div",{className:"storybookContainer",children:r.jsx(o,{})}),argTypes:{children:{if:{arg:"false",eq:"true"}},color:{control:{type:"select"},options:s},variant:{control:{type:"inline-radio"}},ghostHover:{control:{type:"inline-radio"},if:{arg:"variant",eq:"ghost"}},size:{control:{type:"inline-radio"},options:i},radius:{control:{type:"inline-radio"}},loading:{table:{disable:!0}},loadingPosition:{table:{disable:!0}},startIcon:{table:{disable:!0}},endIcon:{table:{disable:!0}},ref:{table:{disable:!0}},as:{table:{disable:!0}},wrapperClasses:{table:{disable:!0}}}},e={render:({...o})=>r.jsx(a,{children:r.jsx(t,{...o})})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  render: ({
-    ...props
-  }) => {
-    return <ThemeContextProvider>\r
-        <DarkModeToggle {...props} />\r
-      </ThemeContextProvider>;
-  }
-}`,...e.parameters?.docs?.source}}};const T=["Default"];export{e as Default,T as __namedExportsOrder,h as default};
---
+++ assets/Intro.stories-CGIkgxQn.js
@@ -0,0 +1,19 @@
+import{j as r}from"./iframe-CpLCljAr.js";import"./Anchor-CTztCqDH.js";import"./Badge-DZ0LU-ER.js";import{B as t}from"./Button-Cg0A6f78.js";import"./ButtonGroup-CVcJpW_t.js";import"./CardTitle-DJM8_RCx.js";import"./DarkModeToggle-BPtAeVqz.js";import"./DarkModeSelector-DmMjxTI6.js";import"./Divider-BIcKdvKl.js";import"./FooterLinksTitle-Ch1kSVC-.js";import"./HamburgerButton-CnaTUysA.js";import{H as e,a as n,b as i,c as s}from"./HeroText-D0hToZ7N.js";import"./Input-C8YAf2wL.js";import"./NavbarToggle-Beim2Hc7.js";import"./Checkbox-CXPAQtz3.js";import"./RadioGroup-XyenOnUX.js";import"./TooltipContent-0KvHkZAo.js";import"./PopoverTrigger-BlSLSjTp.js";import"./DropdownListItem-Da4au42n.js";import"./preload-helper-PPVm8Dsz.js";import"./bundle-mjs-169JcMJR.js";import"./icons-CI5eOsvU.js";import"./floating-ui.react-4xKgWT0x.js";import"./FloatingDelayGroup-DMeSQnUB.js";import"./Slot-Dci64KlD.js";const _={title:"Intro/Welcome"},o={render:()=>r.jsx("div",{className:"bg-main",children:r.jsx(e,{textAlign:"center",className:"h-screen min-h-0",children:r.jsxs(n,{children:[r.jsx(i,{children:"Welcome to my react components library"}),r.jsxs(s,{children:[r.jsx(t,{as:"a",href:"./?path=/story/components",variant:"outline",children:"Components"}),r.jsx(t,{as:"a",href:"./?path=/docs/intro-installation--docs",children:"Installation"})]})]})})})};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    return <div className="bg-main">\r
+        <Hero textAlign="center" className="h-screen min-h-0">\r
+          <HeroSection>\r
+            <HeroTitle>Welcome to my react components library</HeroTitle>\r
+            <HeroActions>\r
+              <Button as="a" href="./?path=/story/components" variant="outline">\r
+                Components\r
+              </Button>\r
+              <Button as="a" href="./?path=/docs/intro-installation--docs">\r
+                Installation\r
+              </Button>\r
+            </HeroActions>\r
+          </HeroSection>\r
+        </Hero>\r
+      </div>;
+  }
+}`,...o.parameters?.docs?.source}}};const O=["Welcome"];export{o as Welcome,O as __namedExportsOrder,_ as default};
