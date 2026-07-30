c54cf85e8f53c989191230155c478cb7f5e20071
Julian <chetkara@gmail.com>
2026-07-31 01:28:59 +0300

    Updates

---
+++ assets/Intro.stories-DeRSPRiw.js
@@ -0,0 +1,19 @@
+import{j as r}from"./iframe-CIDGPNHV.js";import"./Anchor-Ds0f2N5k.js";import"./Badge-Cyc-tfkR.js";import{B as t}from"./Button-MyjY1hD_.js";import"./ButtonGroup-CrUJK2Qw.js";import"./CardTitle-C0GqQpYL.js";import"./DarkModeToggle-BaBCE2AF.js";import"./DarkModeSelector-CfUCPX0N.js";import"./Divider-Bxhroksb.js";import"./FooterLinksTitle-CIM0BMzG.js";import"./HamburgerButton-CRnrvEiP.js";import{H as e,a as n,b as i,c as s}from"./HeroText-D9v2Ckl3.js";import"./Input-BLOw4O1c.js";import"./NavbarToggle-BKceBhP_.js";import"./Checkbox-9HOInYqc.js";import"./RadioGroup-C1_m1Vd_.js";import"./TooltipContent-CV_PBd3Y.js";import"./PopoverTrigger-C6zdQlX6.js";import"./DropdownListItem-BM407l8r.js";import"./SidemenuItem-BMknlwNk.js";import"./preload-helper-PPVm8Dsz.js";import"./bundle-mjs-169JcMJR.js";import"./icons-xD5BLknX.js";import"./FloatingContext-Bxq_MTRA.js";import"./useIsMobile-C_XAMvrW.js";import"./FloatingElementContent-001hAOdR.js";import"./index-DFQY8QEJ.js";const R={title:"Intro/Welcome"},o={render:()=>r.jsx("div",{className:"bg-main",children:r.jsx(e,{textAlign:"center",className:"h-screen min-h-0",children:r.jsxs(n,{children:[r.jsx(i,{children:"Welcome to my react components library"}),r.jsxs(s,{children:[r.jsx(t,{as:"a",href:"./?path=/story/components",variant:"outline",children:"Components"}),r.jsx(t,{as:"a",href:"./?path=/docs/intro-installation--docs",children:"Installation"})]})]})})})};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    return <div className="bg-main">\r
+        <Hero textAlign="center" className="h-screen min-h-0">\r
+          <HeroSection>\r
+            <HeroTitle>Welcome to my react components library</HeroTitle>\r
+            <HeroActions>\r
+              <Button as="a" href="./?path=/story/components" variant="outline">\r
+                Components\r
+              </Button>\r
+              <Button as="a" href="./?path=/docs/intro-installation--docs">\r
+                Installation\r
+              </Button>\r
+            </HeroActions>\r
+          </HeroSection>\r
+        </Hero>\r
+      </div>;
+  }
+}`,...o.parameters?.docs?.source}}};const k=["Welcome"];export{o as Welcome,k as __namedExportsOrder,R as default};
