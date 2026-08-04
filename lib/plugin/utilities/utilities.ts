14f69c4d40019cd3eea4d728415483ff8f695ec8
Julian <chetkara@gmail.com>
2026-08-04 10:35:03 +0200

    adjusted dropdown max size

--- lib/plugin/utilities/utilities.ts
+++ lib/plugin/utilities/utilities.ts
@@ -90,7 +90,7 @@ export default <Record<string, CssInJs>>{
   },
 
   ".floating-container-inner": {
-    "@apply max-h-60 overflow-y-auto scrollbar-thin scrollbar-thumb-(--fg-color) scrollbar-track-(--bg-color)":
+    "@apply max-h-3/4 sm:max-h-80 overflow-y-auto scrollbar-thin scrollbar-thumb-(--fg-color) scrollbar-track-(--bg-color)":
       {},
   },
 
