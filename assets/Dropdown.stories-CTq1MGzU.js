35a849e24e5822254413b354f9eba4878b2471cc
Julian <chetkara@gmail.com>
2026-08-12 16:50:56 +0200

    Updates

---
+++ assets/Dropdown.stories-CTq1MGzU.js
@@ -0,0 +1,136 @@
+import{j as e}from"./iframe-C8KOGUx2.js";import{d as b}from"./index-x6WoGDPP.js";import{b as T,e as I,a as x}from"./index-DttbUWqL.js";import"./Anchor-rFKeucJX.js";import"./Badge-CI2II2jK.js";import{B as g}from"./ButtonGroup-BRq2mKe4.js";import"./PopoverTrigger-CxdIAyBJ.js";import"./DarkModeToggle-tHC05-bQ.js";import"./DarkModeSelector-oDWjdH7u.js";import{d as s,e as c,f as n,g as i,h as d,i as a}from"./Combobox-Cew3lWqX.js";import"./Divider-CP-4uY48.js";import"./HamburgerButton-Bzg7zkAZ.js";import"./AutocompleteInput-BZUGZvUV.js";import"./Checkbox-nTJdBJuY.js";import"./RadioGroup-CdUkHJVG.js";import"./TooltipContent-tg41VTgy.js";import"./SidemenuItem-Bg2UdGfL.js";import"./SelectOption-BEOe9FU9.js";import"./Skeleton-DBNVPYgH.js";import"./preload-helper-PPVm8Dsz.js";import"./cssColorProps-BldbZDxN.js";import"./bundle-mjs-Ct12j0u0.js";import"./icons-C8I_aUnW.js";import"./FloatingElementContent-BfnogGtk.js";import"./useIsMobile-DfGPh0Mw.js";import"./helpers-KWlkL90F.js";const{expect:o,screen:h,userEvent:u,within:y}=__STORYBOOK_MODULE_TEST__,V={title:"Components/Dropdown",tags:["autodocs"],component:a,subcomponents:{DropdownContent:d,DropdownList:i,DropdownListItem:n,DropdownTitle:c,DropdownTrigger:s},decorators:t=>e.jsx("div",{className:"storybookContainer",children:e.jsx(t,{})}),argTypes:{color:{options:x,control:{type:"select"},table:{category:"controls"}},mobileSheetPlacement:{options:I,control:{type:"select"},table:{category:"controls"}},placement:{options:T,control:{type:"select"},table:{category:"controls"}},returnFocus:{control:{type:"boolean"},table:{category:"controls"}},hasArrow:{table:{category:"controls"}},strategy:{table:{category:"controls"}},requireClick:{table:{category:"controls"}},disabled:{table:{category:"controls"}},mobileSheet:{table:{category:"controls"}},isOpen:{control:!1,table:{category:"docs"}},onOpenChange:{control:!1,table:{category:"docs"}}}},l={play:async({canvasElement:t})=>{const r=y(t),w=r.getByText("Open menu");o(r.queryByText("Menu")).toBeNull(),await u.click(w);const D=h.getByText("Menu");o(D,"renders").toBeInTheDocument(),await u.click(document.body),o(r.queryByText("Menu")).toBeNull()},render:({...t})=>e.jsxs(a,{...t,children:[e.jsx(s,{children:e.jsx(g,{children:"Open menu"})}),e.jsxs(d,{children:[e.jsx(c,{children:"Menu"}),e.jsxs(i,{children:[e.jsx(n,{children:"Item 1"}),e.jsx(n,{children:"Item 2"}),e.jsx(n,{children:"Item 3"})]})]})]})},p={play:async({canvasElement:t})=>{const r=y(t),w=r.getByText("Open menu");o(r.queryByText("Menu")).toBeNull(),await u.click(w);const D=h.getByText("Menu");o(D,"renders").toBeInTheDocument(),await u.click(document.body),o(r.queryByText("Menu")).toBeNull()},render:({...t})=>e.jsxs(a,{...t,children:[e.jsx(s,{children:e.jsx(g,{children:"Open menu"})}),e.jsxs(d,{children:[e.jsx(c,{children:"Menu"}),e.jsxs(i,{children:[e.jsx(n,{children:"Item 1"}),e.jsx(n,{disabled:!0,children:"Item 2"}),e.jsxs(a,{placement:"right",children:[e.jsx(s,{children:e.jsx(n,{children:"Item 3"})}),e.jsxs(d,{children:[e.jsx(c,{children:"Sub menu"}),e.jsxs(i,{children:[e.jsx(n,{children:"Nested 1"}),e.jsx(n,{children:"Nested 2"}),e.jsx(n,{children:"Nested 3"})]})]})]}),e.jsxs(n,{className:"justify-start",children:[e.jsx(b,{}),"Item 4"]})]})]})]}),argTypes:{isOpen:{table:{disable:!0}},onOpenChange:{table:{disable:!0}}}},m={render:({...t})=>e.jsx(e.Fragment,{children:x.map(r=>e.jsxs(a,{color:r,...t,children:[e.jsx(s,{children:e.jsx(g,{color:r,children:r})}),e.jsxs(d,{children:[e.jsx(c,{children:"Menu"}),e.jsxs(i,{children:[e.jsx(n,{children:"Item 1"}),e.jsxs(a,{children:[e.jsx(s,{children:e.jsx(n,{children:"Item 2"})}),e.jsx(d,{children:e.jsx(i,{children:e.jsx(n,{children:"Nested"})})})]})]})]})]},r))}),argTypes:{color:{table:{disable:!0}},isOpen:{table:{disable:!0}},onOpenChange:{table:{disable:!0}}}};l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    const trigger = canvas.getByText("Open menu");
+    expect(canvas.queryByText("Menu")).toBeNull();
+    await userEvent.click(trigger);
+    const dropdown = screen.getByText("Menu");
+    expect(dropdown, "renders").toBeInTheDocument();
+    await userEvent.click(document.body);
+    expect(canvas.queryByText("Menu")).toBeNull();
+  },
+  render: ({
+    ...rest
+  }) => {
+    return <Dropdown {...rest}>\r
+        <DropdownTrigger>\r
+          <Button>Open menu</Button>\r
+        </DropdownTrigger>\r
+        <DropdownContent>\r
+          <DropdownTitle>Menu</DropdownTitle>\r
+          <DropdownList>\r
+            <DropdownListItem>Item 1</DropdownListItem>\r
+            <DropdownListItem>Item 2</DropdownListItem>\r
+            <DropdownListItem>Item 3</DropdownListItem>\r
+          </DropdownList>\r
+        </DropdownContent>\r
+      </Dropdown>;
+  }
+}`,...l.parameters?.docs?.source}}};p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    const trigger = canvas.getByText("Open menu");
+    expect(canvas.queryByText("Menu")).toBeNull();
+    await userEvent.click(trigger);
+    const dropdown = screen.getByText("Menu");
+    expect(dropdown, "renders").toBeInTheDocument();
+    await userEvent.click(document.body);
+    expect(canvas.queryByText("Menu")).toBeNull();
+  },
+  render: ({
+    ...rest
+  }) => {
+    return <Dropdown {...rest}>\r
+        <DropdownTrigger>\r
+          <Button>Open menu</Button>\r
+        </DropdownTrigger>\r
+        <DropdownContent>\r
+          <DropdownTitle>Menu</DropdownTitle>\r
+          <DropdownList>\r
+            <DropdownListItem>Item 1</DropdownListItem>\r
+            <DropdownListItem disabled>Item 2</DropdownListItem>\r
+            <Dropdown placement="right">\r
+              <DropdownTrigger>\r
+                <DropdownListItem>Item 3</DropdownListItem>\r
+              </DropdownTrigger>\r
+              <DropdownContent>\r
+                <DropdownTitle>Sub menu</DropdownTitle>\r
+                <DropdownList>\r
+                  <DropdownListItem>Nested 1</DropdownListItem>\r
+                  <DropdownListItem>Nested 2</DropdownListItem>\r
+                  <DropdownListItem>Nested 3</DropdownListItem>\r
+                </DropdownList>\r
+              </DropdownContent>\r
+            </Dropdown>\r
+            <DropdownListItem className="justify-start">\r
+              <PiGearThin />\r
+              Item 4\r
+            </DropdownListItem>\r
+          </DropdownList>\r
+        </DropdownContent>\r
+      </Dropdown>;
+  },
+  argTypes: {
+    isOpen: {
+      table: {
+        disable: true
+      }
+    },
+    onOpenChange: {
+      table: {
+        disable: true
+      }
+    }
+  }
+}`,...p.parameters?.docs?.source}}};m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  render: ({
+    ...rest
+  }) => {
+    return <>\r
+        {availableColors.map(color => {
+        return <Dropdown key={color} color={color} {...rest}>\r
+              <DropdownTrigger>\r
+                <Button color={color}>{color}</Button>\r
+              </DropdownTrigger>\r
+              <DropdownContent>\r
+                <DropdownTitle>Menu</DropdownTitle>\r
+                <DropdownList>\r
+                  <DropdownListItem>Item 1</DropdownListItem>\r
+                  <Dropdown>\r
+                    <DropdownTrigger>\r
+                      <DropdownListItem>Item 2</DropdownListItem>\r
+                    </DropdownTrigger>\r
+                    <DropdownContent>\r
+                      <DropdownList>\r
+                        <DropdownListItem>Nested</DropdownListItem>\r
+                      </DropdownList>\r
+                    </DropdownContent>\r
+                  </Dropdown>\r
+                </DropdownList>\r
+              </DropdownContent>\r
+            </Dropdown>;
+      })}\r
+      </>;
+  },
+  argTypes: {
+    color: {
+      table: {
+        disable: true
+      }
+    },
+    isOpen: {
+      table: {
+        disable: true
+      }
+    },
+    onOpenChange: {
+      table: {
+        disable: true
+      }
+    }
+  }
+}`,...m.parameters?.docs?.source}}};const W=["Default","Nested","Colors"];export{m as Colors,l as Default,p as Nested,W as __namedExportsOrder,V as default};
---
+++ assets/Hero.stories-B6h_IrcR.js
@@ -0,0 +1,58 @@
+import{j as e}from"./iframe-C8KOGUx2.js";import{d as p}from"./index-DttbUWqL.js";import"./Anchor-rFKeucJX.js";import"./Badge-CI2II2jK.js";import{B as t}from"./ButtonGroup-BRq2mKe4.js";import"./PopoverTrigger-CxdIAyBJ.js";import"./DarkModeToggle-tHC05-bQ.js";import"./DarkModeSelector-oDWjdH7u.js";import{r as s,b as n,a,s as l,c as m,H as c}from"./Combobox-Cew3lWqX.js";import"./Divider-CP-4uY48.js";import"./HamburgerButton-Bzg7zkAZ.js";import"./AutocompleteInput-BZUGZvUV.js";import"./Checkbox-nTJdBJuY.js";import"./RadioGroup-CdUkHJVG.js";import"./TooltipContent-tg41VTgy.js";import"./SidemenuItem-Bg2UdGfL.js";import"./SelectOption-BEOe9FU9.js";import"./Skeleton-DBNVPYgH.js";import"./preload-helper-PPVm8Dsz.js";import"./cssColorProps-BldbZDxN.js";import"./bundle-mjs-Ct12j0u0.js";import"./icons-C8I_aUnW.js";import"./FloatingElementContent-BfnogGtk.js";import"./useIsMobile-DfGPh0Mw.js";import"./helpers-KWlkL90F.js";const _={title:"Components/Hero",tags:["autodocs"],component:c,subcomponents:{HeroActions:m,HeroImageSection:l,HeroSection:a,HeroTitle:n,HeroText:s},argTypes:{children:{table:{disable:!0}},textAlign:{control:{type:"inline-radio"},table:{category:"controls"}},actionsAlign:{control:{type:"inline-radio"},table:{category:"controls"}},responsiveAt:{control:{type:"inline-radio"},options:p,table:{category:"controls"}},wrapperClassName:{control:!1,table:{category:"docs"}},wrapperStyle:{control:!1,table:{category:"docs"}}}},r={render:({...i})=>e.jsx("div",{className:"bg-main",children:e.jsx(c,{...i,children:e.jsxs(a,{children:[e.jsx(n,{children:"Lorem ipsum dolor sit amet."}),e.jsx(s,{children:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore ex, corporis rem dolores culpa quo corrupti doloremque minima excepturi molestiae."}),e.jsxs(m,{children:[e.jsx(t,{children:"action"}),e.jsx(t,{children:"action"})]})]})})})},o={render:({...i})=>e.jsx("div",{className:"bg-main",children:e.jsxs(c,{...i,children:[e.jsxs(a,{className:"max-md:h-1/2 md:w-1/2",children:[e.jsx(n,{children:"Lorem ipsum dolor sit amet."}),e.jsx(s,{children:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore ex, corporis rem dolores culpa quo corrupti doloremque minima excepturi molestiae."}),e.jsxs(m,{children:[e.jsx(t,{children:"action"}),e.jsx(t,{children:"action"})]})]}),e.jsx(l,{className:"max-md:h-1/2 md:w-1/2",children:e.jsx("img",{className:"hero-image md:w-[50vw]",src:"https://picsum.photos/1400/1400",alt:"placeholder image"})})]})}),argTypes:{wrapperClassName:{table:{disable:!0}},wrapperStyle:{table:{disable:!0}}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: ({
+    ...props
+  }) => {
+    return <div className="bg-main">\r
+        <Hero {...props}>\r
+          <HeroSection>\r
+            <HeroTitle>Lorem ipsum dolor sit amet.</HeroTitle>\r
+            <HeroText>\r
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore\r
+              ex, corporis rem dolores culpa quo corrupti doloremque minima\r
+              excepturi molestiae.\r
+            </HeroText>\r
+            <HeroActions>\r
+              <Button>action</Button>\r
+              <Button>action</Button>\r
+            </HeroActions>\r
+          </HeroSection>\r
+        </Hero>\r
+      </div>;
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  render: ({
+    ...props
+  }) => {
+    return <div className="bg-main">\r
+        <Hero {...props}>\r
+          <HeroSection className="max-md:h-1/2 md:w-1/2">\r
+            <HeroTitle>Lorem ipsum dolor sit amet.</HeroTitle>\r
+            <HeroText>\r
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore\r
+              ex, corporis rem dolores culpa quo corrupti doloremque minima\r
+              excepturi molestiae.\r
+            </HeroText>\r
+            <HeroActions>\r
+              <Button>action</Button>\r
+              <Button>action</Button>\r
+            </HeroActions>\r
+          </HeroSection>\r
+          <HeroImageSection className="max-md:h-1/2 md:w-1/2">\r
+            <img className="hero-image md:w-[50vw]" src="https://picsum.photos/1400/1400" alt="placeholder image" />\r
+          </HeroImageSection>\r
+        </Hero>\r
+      </div>;
+  },
+  argTypes: {
+    wrapperClassName: {
+      table: {
+        disable: true
+      }
+    },
+    wrapperStyle: {
+      table: {
+        disable: true
+      }
+    }
+  }
+}`,...o.parameters?.docs?.source}}};const z=["Default","WithImage"];export{r as Default,o as WithImage,z as __namedExportsOrder,_ as default};
