902320c68377c3fdb23d655917e1b7a0711e335f
barrelrolla <chetkara@gmail.com>
2026-08-21 09:51:05 +0300

    removed forgotten export

--- lib/components/Popover/index.ts
+++ lib/components/Popover/index.ts
@@ -4,4 +4,3 @@ export { PopoverContent } from "./PopoverContent";
 export { PopoverTrigger } from "./PopoverTrigger";
 export type { PopoverTriggerProps } from "./PopoverTrigger";
 export { PopoverContextProvider, usePopoverContext } from "./PopoverContext";
-export type { PopoverContextType } from "./PopoverContext";
